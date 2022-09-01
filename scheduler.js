079e537fac5146da6372e7b4dee9fbe62ac1dcd1
LAPTOP-RGABST8T\Alex <broa29@hotmail.com>
2022-08-31 22:31:50 -0400

    WIP scheduler changes, created a Batch object, currently only reproduces the old behavior

--- scheduler.js
+++ scheduler.js
@@ -4,17 +4,15 @@ const W1 = 1;
 const G = 2;
 const W2 = 3;
 
-// Port used by the worker scripts to report their death to this script
-const PORT = 1;
-
-// Amount of time between jobs. The same spacer is used between batches as well.
-const SPACER = 30;
-
-let g_currentId= 0;
-
 export async function main(ns) {
 	ns.disableLog('ALL');
 	ns.tail();
+
+	// Amount of time between jobs. The same spacer is used between batches as well.
+	const SPACER = 30;
+
+	// Port used by the worker scripts to report their death to this script
+	const PORT = 1;
 
 	// We declare a clock object
 	const clock = new ClockSync(ns);
@@ -36,7 +34,11 @@ export async function main(ns) {
 		// to simulate each type of job
 		"times": [524, 2094, 1675, 2094],
 		// This is how long we need to wait from batch start to kick each job
-		"delays": [1540, 0, 449, 60]
+		"delays": [1540, 0, 449, 60],
+		// Delay between jobs
+		delay: SPACER,
+		// Tolerance
+		tolerance: SPACER / 1.5
 	};
 
 	// Simply an array to track batches. We currently never purge this array so don't leave it running too long or it will pile up
@@ -49,23 +51,12 @@ export async function main(ns) {
 	while (true) {
 		// Look for 'Batch' tasks and add some if we're under the target. We're using a target of 1 for this test.
 		while (clock.tasks.filter(t => t.desc.startsWith('Batch')).length < 2) {
-			// Adds the new task in the scheduler
-			clock.AddTask(g_currentId++, `Batch ${id}`, nextBatch, SPACER - 10, () => StartBatch(ns, id, metrics, PORT, clock), []);
+			const batch = new Batch(ns, id++, metrics);
+			batches.push(batch);
+			batch.Schedule(clock, nextBatch, PORT);
 
 			// The next batch time is based on the previous batch, not the current time
 			nextBatch += windowLen;
-
-			// Correct any significant offset we might have
-			while (nextBatch < performance.now()) {
-				nextBatch += windowLen;
-				ns.print('WARN: Skipping ahead, we are lagging behind!');
-			}
-
-			// Add a placeholder object in our batches array to keep track of job ends (see how it's used in WorkerDeathReports())
-			batches.push({ id: id, replies: '', replyCount: 0 });
-
-			// Increase the batch id for the next batch
-			id++;
 		}
 
 		// This simply processes the task queue and runs what needs to be run
@@ -86,14 +77,78 @@ export async function main(ns) {
 
 // WIP
 class Batch {
-	constructor() {
-		this.scheduledTime= 0;
-		this.execTime= 0;
-		this.workerStartTime= [0,0,0,0];
-		this.workerEndTime= [0,0,0,0];
-		this.reportedTime [0,0,0,0];
-		this.started= false;
-		this.aborted= false;
+	constructor(ns, id, metrics) {
+		this.ns = ns;
+		this.id = id;
+		this.metrics = metrics;
+
+		this.clock = undefined;
+		this.port = undefined;
+
+		this.scheduledTime = 0;	// Time when the batch is meant to start (performance.now() based)
+		this.execTime = 0;		// Actual time when the batch was allowed to start ((performance.now() based))
+
+		this.started = false;	// Indicates whether or not we've started running this batch
+		this.aborted = false;	// Indicates whether or not this batch was completely or partially aborted
+
+		this.reports = [];
+		this.workerAborted = [false, false, false, false];	// Indicates whether or not each worker has been aborted
+	}
+
+	Schedule(clock, time, port) {
+		this.clock = clock;
+		this.port = port;
+		this.scheduledTime = time;
+
+		// Adds the new task in the scheduler
+		this.clock.AddTask(`Batch ${this.id}`, this.scheduledTime, this.metrics.delay, () => this.Start(), []);
+	}
+
+	// Test function to start a mock batch. It simply adds them as tasks in the ClockSync instance.
+	Start() {
+		let now = performance.now();
+		this.clock.AddTask(`${this.id}.H`, now + this.metrics.delays[H], this.metrics.tolerance, () => this.StartJob(H, this.metrics.times[H]), []);
+		this.clock.AddTask(`${this.id}.W1`, now + this.metrics.delays[W1], this.metrics.tolerance, () => this.StartJob(W1, this.metrics.times[W1]), []);
+		this.clock.AddTask(`${this.id}.G`, now + this.metrics.delays[G], this.metrics.tolerance, () => this.StartJob(G, this.metrics.times[G]), []);
+		this.clock.AddTask(`${this.id}.W2`, now + this.metrics.delays[W2], this.metrics.tolerance, () => this.StartJob(W2, this.metrics.times[W2]), []);
+	}
+
+	// Test function to start a mock job. The script emulates H/W/G without actually doing anything to the server
+	// Desyncs are inconsequential, since they do not affect any servers. The script just sleeps for the duration to simulate
+	// a hack, grow or weaken call
+	StartJob(type, duration) {
+		this.ns.exec('fakejob.js', 'home', 1, this.id, type, duration, this.port);
+	}
+
+	ValidateReports() {
+		//let aborted= this.workerAborted.reduce((sum, s) => sum + (s == true ? 1 : 0), 0);
+
+		if (this.reports.length != 4) return;
+
+		let replyChain = [];
+		for (const report of this.reports) {
+			switch (report.type) {
+				case H:
+					replyChain.push('H');
+					break;
+				case W1:
+					replyChain.push('W1');
+					break;
+				case G:
+					replyChain.push('G');
+					break;
+				case W2:
+					replyChain.push('W2');
+					break;
+			}
+		}
+
+		if (replyChain.toString() == 'H,W1,G,W2') {
+			this.ns.print('SUCCESS: Batch ' + this.id + ' finished in correct order');
+		}
+		else {
+			this.ns.print('FAIL: Batch ' + this.id + ' finished out of order ' + replyChain.toString());
+		}
 	}
 }
 
@@ -117,16 +172,9 @@ function WorkerDeathReports(ns, port, batches) {
 			continue;
 		}
 
-		batch.replyCount++;
-		batch.replies = batch.replies + data.type;
-		if (batch.replyCount == 4) {
-			if (batch.replies == 'H W1G W2') {
-				ns.print('SUCCESS: Batch ' + data.id + ' finished in correct order');
-			}
-			else {
-				ns.print('FAIL: Batch ' + data.id + ' finished out of order ' + batch.replies);
-			}
-		}
+		data.reported = performance.now();
+		batch.reports.push(data);
+		batch.ValidateReports();
 	}
 }
 
@@ -142,6 +190,7 @@ class ClockSync {
 		this.lastProc = performance.now();
 		this.drift = 0;
 		this.ns = ns;
+		this.nextTid = 0;
 	}
 
 	Process() {
@@ -185,7 +234,6 @@ class ClockSync {
 	// Adds a task to the queue
 	// 
 	AddTask(
-		id,  		// Task id
 		desc,  		// Task description, not important
 		time, 		// Time at which we want to start the task (relative to performance.now())
 		tolerance, 	// How much further than 'time' we allow the task to be started. If we get to it beyond this time, it will be cancelled.
@@ -193,7 +241,7 @@ class ClockSync {
 		args		// Arguments to be passed to the function (pass an empty array if none are needed)
 	) {
 		let task = {
-			id: id,
+			id: this.nextTid++,
 			desc: desc,
 			time: time,
 			tolerance: tolerance,
@@ -203,6 +251,7 @@ class ClockSync {
 			started: null
 		};
 		this.tasks.push(task);
+		return task.id;
 	}
 
 	// Removes tasks that are started or aborted to keep the queue mean and lean
@@ -211,18 +260,18 @@ class ClockSync {
 	}
 }
 
-// Test function to start a mock batch. It simply adds them as tasks in the ClockSync instance.
-function StartBatch(ns, id, metrics, port, clock) {
-	let now = performance.now();
-	clock.AddTask(g_currentId++, `${id}.H`, now + metrics.delays[H], SPACER - 10, () => StartJob(ns, 'H ', id, metrics.times[H], port), []);
-	clock.AddTask(g_currentId++, `${id}.W1`, now + metrics.delays[W1], SPACER - 10, () => StartJob(ns, 'W1', id, metrics.times[W1], port), []);
-	clock.AddTask(g_currentId++, `${id}.G`, now + metrics.delays[G], SPACER - 10, () => StartJob(ns, 'G ', id, metrics.times[G], port), []);
-	clock.AddTask(g_currentId++, `${id}.W2`, now + metrics.delays[W2], SPACER - 10, () => StartJob(ns, 'W2', id, metrics.times[W2], port), []);
-}
+// // Test function to start a mock batch. It simply adds them as tasks in the ClockSync instance.
+// function StartBatch(ns, id, metrics, port, clock) {
+// 	let now = performance.now();
+// 	clock.AddTask(`${id}.H`, now + metrics.delays[H], SPACER - 10, () => StartJob(ns, 'H ', id, metrics.times[H], port), []);
+// 	clock.AddTask(`${id}.W1`, now + metrics.delays[W1], SPACER - 10, () => StartJob(ns, 'W1', id, metrics.times[W1], port), []);
+// 	clock.AddTask(`${id}.G`, now + metrics.delays[G], SPACER - 10, () => StartJob(ns, 'G ', id, metrics.times[G], port), []);
+// 	clock.AddTask(`${id}.W2`, now + metrics.delays[W2], SPACER - 10, () => StartJob(ns, 'W2', id, metrics.times[W2], port), []);
+// }
 
-// Test function to start a mock job. The script emulates H/W/G without actually doing anything to the server
-// Desyncs are inconsequential, since they do not affect any servers. The script just sleeps for the duration to simulate
-// a hack, grow or weaken call
-function StartJob(ns, type, id, duration, port) {
-	ns.exec('fakejob.js', 'home', 1, id, type, duration, port);
-}
+// // Test function to start a mock job. The script emulates H/W/G without actually doing anything to the server
+// // Desyncs are inconsequential, since they do not affect any servers. The script just sleeps for the duration to simulate
+// // a hack, grow or weaken call
+// function StartJob(ns, type, id, duration, port) {
+// 	ns.exec('fakejob.js', 'home', 1, id, type, duration, port);
+// }

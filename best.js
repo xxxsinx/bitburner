b61661770af1a7abf32e9845469193ff20c5f8de
LAPTOP-RGABST8T\Alex <broa29@hotmail.com>
2022-09-20 18:10:46 -0400

    Updated best to use my new Weight function

--- best.js
+++ best.js
@@ -1,11 +1,48 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	let servers= GetAllServers(ns);
-	servers= servers.filter(s=> ns.getServerRequiredHackingLevel(s) <= ns.getPlayer().skills.hacking / 2 && ns.hasRootAccess(s) && ns.getServerMaxMoney(s) > 0);
-	servers.sort((a,b) => ns.getServerMaxMoney(a) / ns.getServerMinSecurityLevel(a) - ns.getServerMaxMoney(b) / ns.getServerMinSecurityLevel(b));
-	if (servers.length == 0) servers.push('n00dles');
-	let bestServer= servers.pop();
-	ns.tprint('Best server is ' + bestServer);
+	let servers = GetAllServers(ns).filter(s => Weight(ns, s) > 0);
+	servers.sort((a, b) => Weight(ns, b) - Weight(ns, a));
+
+	ns.tprint('Best servers: ');
+	for (let server of servers) {
+		ns.tprint(server.padEnd(20) + ': ' + Weight(ns, server));
+	}
+}
+
+// Returns a weight that can be used to sort servers by hack desirability
+function Weight(ns, server) {
+	if (!server) return 0;
+
+	// Don't ask, endgame stuff
+	if (server.startsWith('hacknet-node')) return 0;
+
+	// Get the player information
+	let player = ns.getPlayer();
+
+	// Get the server information
+	let so = ns.getServer(server);
+
+	// Set security to minimum on the server object (for Formula.exe functions)
+	so.hackDifficulty = so.minDifficulty;
+
+	// We cannot hack a server that has more than our hacking skill so these have no value
+	if (so.requiredHackingSkill > player.skills.hacking) return 0;
+
+	// Default pre-Formulas.exe weight. minDifficulty directly affects times, so it substitutes for min security times
+	let weight = so.moneyMax / so.minDifficulty;
+
+	// If we have formulas, we can refine the weight calculation
+	if (ns.fileExists('Formulas.exe')) {
+		// We use weakenTime instead of minDifficulty since we got access to it, 
+		// and we add hackChance to the mix (pre-formulas.exe hack chance formula is based on current security, which is useless)
+		weight = so.moneyMax / ns.formulas.hacking.weakenTime(so, player) * ns.formulas.hacking.hackChance(so, player);
+	}
+	else
+		// If we do not have formulas, we can't properly factor in hackchance, so we lower the hacking level tolerance by half
+		if (so.requiredHackingSkill > player.skills.hacking / 2)
+			return 0;
+
+	return weight;
 }
 
 // Recursive network scan, compressed

fc9b39c85b687bf3b9f20bd70084cce3b36f0bea
Alex <30581136+xxxsinx@users.noreply.github.com>
2022-10-17 22:48:14 -0400

    ka.js revisited and fixed

--- ka.js
+++ ka.js
@@ -1,25 +1,8 @@
+import { GetAllServers } from 'utils.js'
+
 export async function main(ns) {
-	for (var server of RecursiveScan(ns)) {
-		await ns.scriptKill('weaken.js', server);
-		await ns.scriptKill('grow.js', server);
-		await ns.scriptKill('hack.js', server);
-		await ns.scriptKill('weaken-once.js', server);
-		await ns.scriptKill('grow.js-once.js', server);
-		await ns.scriptKill('hack.js-once.js', server);
-		await ns.scriptKill('dothemagic.js', server);
+	for (var server of GetAllServers(ns)) {
+		ns.killall(server, true);
 	}
-
-	await ns.print('Killed all proceeses');
-}
-
-function RecursiveScan(ns, root, found) {
-	if (found == null) found = new Array();
-	if (root == null) root = 'home';
-	if (found.find(p => p == root) == undefined) {
-		found.push(root);
-		for (const server of ns.scan(root))
-			if (found.find(p => p == server) == undefined)
-				RecursiveScan(ns, server, found);
-	}
-	return found;
+	ns.tprint('WARN: Killed all scripts!');
 }
